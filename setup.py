1024efae219ee4726ddd0b7cb8051e6938ffafa9
Dee0105 <bdeeptimayee966@gmail.com>
2026-02-23 22:44:33 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_package, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Deepti",
+    author_email="bdeeptimayee966@gmail.com",
+    packages=find_package(),
+    install_requires=[]
+)
